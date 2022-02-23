ce0940106b39ab302d258c0411316e57ac2a4d6b
Gilberto <gilbertocruz2007@micorreo.upp.edu.mx>
2022-02-23 15:22:07 -0600

    Realize todos los cambios necesarios en el controlador

--- Controlador/ControladorMDB.java
+++ Controlador/ControladorMDB.java
@@ -10,30 +10,43 @@ package Controlador;
  * @author Stevia
  */
 import Modelo.MDB;
-import Vista.FrmVista;
+//import Vista.FrmVista;
+import Vista.FrmVista2; //Importe El Jframe
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
 
 public class ControladorMDB implements ActionListener{
-  private FrmVista _view; //el formulario 
+  //private FrmVista _view; //el formulario 
+  private FrmVista2 _view; //El formulario del Jframe 
   private MDB _model;//el manejador de bases de datos
 
     
-    public ControladorMDB(FrmVista view, MDB model){
+    //public ControladorMDB(FrmVista view, MDB model){
+    public ControladorMDB(FrmVista2 view, MDB model){ //Para el formulario del Jframe
        this._model=model;
        this._view=view;
-        this._view.cmdAlta.addActionListener(this);//se obtiene el control sobre el botón alta del formulario
-this._view.cmdEliminar.addActionListener(this);//se obtiene el control de botón eliminar
+        /*this._view.cmdAlta.addActionListener(this);//se obtiene el control sobre el botón alta del formulario
+this._view.cmdEliminar.addActionListener(this);//se obtiene el control de botón eliminar */
+        this._view.comandSubir.addActionListener(this);
+        this._view.comandEliminar.addActionListener(this);
 }
 @Override
 //En este método se ΗatrapaΗ la interacción del usuario
 public void actionPerformed(ActionEvent e) {
         String leyenda="";//esta variable servirá para atrapar los errores sql
 
-        if (e.getSource()==_view.cmdAlta){ //Si el botón presionado fue el de alta
+        //if (e.getSource()==_view.cmdAlta){ //Si el botón presionado fue el de alta
+        if (e.getSource()==_view.comandSubir){   
          // System.out.print("paso");
-        leyenda=_model.registrarAlta("tbusuarios"," null, ' " +_view.txtNombre.getText()+" ',' " +_view.txtUsuario.getText() +" ', "+_view.txtpsw.getText() );
+        //leyenda=_model.registrarAlta("tbusuarios"," null, ' " +_view.txtNombre.getText()+" ',' " +_view.txtUsuario.getText() +" ', "+_view.txtpsw.getText() );
+        
+        leyenda=_model.registrarAlta("seguimiento_clientes"," null, "+_view.textCedula.getText()+" ,' "+_view.textExpedicion.getText()+" ',' "+_view.textNombre.getText() +" ',' "+_view.TextProfesion.getText() +" ',' "+_view.TextDResidencia.getText()
+                +" ',' "+_view.TextCiudad.getText() +" ',' "+_view.TextDepartamento.getText()+" ',' "+_view.TextEmail.getText() +" ', "+_view.TextTResidencia.getText() +" , "+_view.TextTTrabajo.getText() +" , "
+                +_view.TextNFactura.getText() +" ,' "+_view.TextTCartera.getText()+" ',' "+_view.TextFechaM.getText() +" ', "+_view.TextDiasM.getText() +" , "+_view.TextValM.getText() +" , "+_view.TextVSeguro.getText() 
+                +" , "+_view.TextInteresM.getText() +" , "+_view.TextSRestaurant.getText() +" ,' "+_view.TextTCobro.getText() +" ',' "+_view.textFAUno.getText() +" ',' "+_view.textAUno.getText() +" ',' "
+                +_view.textFADos.getText() +" ',' " +_view.textADos.getText() +" ',' "+_view.textFATres.getText() +" ',' "+_view.textATres.getText() +" ',' "+_view.textFACuatro.getText()+" ',' "+_view.textACuatro.getText()+" ' ");
+        
         //Se puede usar una variable o el control, lo ideal es que se pudiera llamar una función de
        // validación aquí sobre los datos obteindos de las cajas de texto
        System.out.print (leyenda);
@@ -47,9 +60,36 @@ public void iniciar(){
 _view.setTitle("MVC_Visual");
 }
 public void limpiar(){
-_view.txtNombre.setText(null);
+/*_view.txtNombre.setText(null);
 _view.txtUsuario.setText(null);
 
-_view.txtpsw.setText(null);
+_view.txtpsw.setText(null);*/
+_view.textCedula.setText(null);
+_view.textExpedicion.setText(null);
+_view.textNombre.setText(null);
+_view.TextProfesion.setText(null);
+_view.TextDResidencia.setText(null);
+_view.TextCiudad.setText(null);
+_view.TextDepartamento.setText(null);
+_view.TextEmail.setText(null);
+_view.TextTResidencia.setText(null);
+_view.TextTTrabajo.setText(null);
+_view.TextNFactura.setText(null);
+_view.TextTCartera.setText(null);
+_view.TextFechaM.setText(null);
+_view.TextDiasM.setText(null);
+_view.TextValM.setText(null);
+_view.TextVSeguro.setText(null);
+_view.TextInteresM.setText(null);
+_view.TextSRestaurant.setText(null);
+_view.TextTCobro.setText(null);
+_view.textFAUno.setText(null);
+_view.textAUno.setText(null);
+_view.textFADos.setText(null);
+_view.textADos.setText(null);
+_view.textFATres.setText(null);
+_view.textATres.setText(null);
+_view.textFACuatro.setText(null);
+_view.textACuatro.setText(null);
 }
 }
