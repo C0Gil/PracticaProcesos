a4560e835b24e2116d9e4a4c19ac7c76871fb7fe
Gilberto <gilbertocruz2007@micorreo.upp.edu.mx>
2022-02-23 14:59:44 -0600

    Volvi comentario a la linea 20 y descomente la linea 17 (lee la linea 17)

--- Modelo/MDB.java
+++ Modelo/MDB.java
@@ -14,10 +14,10 @@ public class MDB {
         public MDB() {
            // conexion = new Conexion("com.mysql.jdbc.Driver", "administrador", "jdbc:mysql://localhost/bdcostos", "root");
 	   //Conexion(String controlador, String contrasena, String URL, String usuario)
-        //conexion = new Conexion("com.mysql.jdbc.Driver",  "jdbc:mysql://127.0.0.1:3306/bddatos", "root","administrador");
+        conexion = new Conexion("com.mysql.jdbc.Driver",  "jdbc:mysql://127.0.0.1:3306/bddatos", "root","aqui colocaras la scontraseña de tu sql local");
 	//public Conexion(String controlador, String contrasena, String URL, String usuario) {
 	//	conexion = new Conexion("com.mysql.jdbc.Driver",  "jdbc:mysql://localhost:3306/bdcostos?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&useTimezone=true&serverTimezone=GMT&useSSL=false", "root","administrador");
-        conexion = new Conexion("com.mysql.jdbc.Driver",  "jdbc:mysql://sql5.freesqldatabase.com:3306/sql5473027", "sql5473027","ZDEJaYyNn8");
+        //conexion = new Conexion("com.mysql.jdbc.Driver",  "jdbc:mysql://sql5.freesqldatabase.com:3306/sql5473027", "sql5473027","ZDEJaYyNn8");
 
         }
 
